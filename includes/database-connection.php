cb148ee43a67e46b14ce5deb59d91808bcca2173
Zach <zachary_breene@uri.edu>
2024-04-23 17:09:48 -0400

    Fixing database-connection

--- includes/database-connection.php
+++ includes/database-connection.php
@@ -2,12 +2,12 @@
 
 $type     = 'mysql';                             // Type of database
 $server   = '192.185.2.183';                    // Server the database is on
-$db       = 'cwpolase_FinancialTracker';     // Name of the database
+$db       = 'zacharyb_financeTracker';     // Name of the database
 $port     = '3306';                           // Port is usually 3306 in Hostgator
 $charset  = 'utf8mb4';                       // UTF-8 encoding using 4 bytes of data per char
 
-$username = 'cwpolase_Wyatt';     // Enter YOUR cPanel username and user here
-$password = 'Gizmo1970';           // Enter YOUR user password here
+$username = 'zacharyb_zbreene436';     // Enter YOUR cPanel username and user here
+$password = 'BFRWXmpz1234';           // Enter YOUR user password here
 
 
 
